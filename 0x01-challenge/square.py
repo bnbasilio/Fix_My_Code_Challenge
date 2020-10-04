4d058d885f1c6d4d2126203b8865f466ad9e4a5d
Nic Basilio <bnbasilio@gmail.com>
2020-10-04 21:52:54 +0000

    added documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+""" square class"""
 
 class square:
+    """ defines the square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ initializes a square object """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +16,11 @@ class square:
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String format of the square """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
