e7775ad702601260d4b771ee5bf32e8a9c1e4be5
Nic Basilio <bnbasilio@gmail.com>
2020-10-04 22:10:39 +0000

    back to rectangle, move height and width to init

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,25 +4,25 @@
 
 class square:
     """ defines the square class """
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         """ initializes a square object """
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_square(self):
         """ Perimeter of the square """
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String format of the square """
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
